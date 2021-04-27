78b894b550131719c3d4a0bf9ad246cbc573c3dd
justin <Yt123>
2021-04-27 18:03:26 +0800

    添加智能客服连接

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,48 @@
 <template>
-    <div id="app" v-cloak>
-        <loading></loading>
-        <layout-header></layout-header>
-        <layout-body></layout-body>
-        <layout-footer></layout-footer>
-    </div>
+  <div id="app" v-cloak>
+    <loading></loading>
+    <layout-header></layout-header>
+    <layout-body></layout-body>
+    <layout-footer></layout-footer>
+  </div>
 </template>
 <script>
-    import layoutHeader from '@/components/layout/layout-header'
-    import layoutBody from '@/components/layout/layout-body'
-    import layoutFooter from '@/components/layout/layout-footer'
-    import Loading from '@/components/loading'
-    export default {
-        name: "app",
-        components: {
-            layoutHeader,
-            layoutBody,
-            layoutFooter,
-            Loading
-        }
+import layoutHeader from '@/components/layout/layout-header'
+import layoutBody from '@/components/layout/layout-body'
+import layoutFooter from '@/components/layout/layout-footer'
+import Loading from '@/components/loading'
+
+export default {
+  name: "app",
+  components: {
+    layoutHeader,
+    layoutBody,
+    layoutFooter,
+    Loading
+  },
+  beforeMount() {
+    // CreateChatBox
+      if(!window.init){
+        this.openChatCustomer()
+      }
+  },
+  methods: {
+    async openChatCustomer() {
+      if (window.init) {
+        window.openCustomer()
+      } else {
+        setTimeout(() => {
+          this.openChatCustomer()
+        }, 2000)
+
+      }
     }
+  }
+}
 </script>
 <style lang="less">
-    #app {
-        font-size: 17px;
-        color: #6f6f6f;
-    }
+#app {
+  font-size: 17px;
+  color: #6f6f6f;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -5,13 +5,11 @@ import store from './store'
 import './assets/css/style.less'
 import './assets/font/iconfont.css'
 import {parseTime} from './utils'
-// 引入makrdown插件
-import mavonEditor from 'mavon-editor'
+import mavonEditor from 'mavon-editor'// 引入makrdown插件
 import 'mavon-editor/dist/css/index.css'
 
 require('./Mock');
 Vue.use(mavonEditor);
-
 Vue.config.productionTip = false;
 Vue.filter('parseTime', (v) => parseTime(v, '{y}-{m}-{d}'));
 new Vue({
