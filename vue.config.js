61770d6ebe81d82d85fdc94a120bf74a15b24603
justin <Yt123>
2021-06-04 11:15:51 +0800

    添加gzip压缩

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
+
+const CompressionWebpackPlugin = require("compression-webpack-plugin");
+// 可加入需要的其他文件类型，比如json
+// 图片不要压缩，体积会比原来还大
+const productionGzipExtensions = ["js", "css"];
+
 module.exports = {
   devServer: {
     port: 8888,
@@ -18,6 +24,21 @@ module.exports = {
       .use("markdown-loader")
       .loader("markdown-loader")
       .end()
-    
+
+  },
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === "production") {
+      return {
+        plugins: [
+          new CompressionWebpackPlugin({
+            // filename: '[path].gz[query]',
+            algorithm: "gzip",
+            test: new RegExp("\\.(" + productionGzipExtensions.join("|") + ")$"),
+            threshold: 10240, //对超过10k的数据进行压缩
+            minRatio: 0.6 // 压缩比例，值为0 ~ 1
+          })
+        ]
+      };
+    }
   }
 };
